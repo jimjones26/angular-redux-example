b85ed27858ae91b0eb2b62723fe1f9ebab14f246
jimjones26 <jimjones26@gmail.com>
2017-03-18 20:25:40 -0400

    a bit complicated for me

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { HttpModule } from '@angular/http';
+import { NgReduxModule } from '@angular-redux/store';
+import { NgReduxRouterModule } from '@angular-redux/router';
 
+// This app's ngModules
+import { StoreModule } from './store/store.module';
+import { AnimalModule } from './animals/animal.module';
+
+// Top-level app component constructs.
+import { appRoutes } from './app.routes';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,9 +19,12 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    StoreModule,
+    AnimalModule
   ],
   providers: [],
   bootstrap: [AppComponent]
